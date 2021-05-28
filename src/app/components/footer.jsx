1e09f597e5afd0d3b4b38c2aa645d67659cb8498
Luis G. Villegas <luisguve@gmail.com>
2021-05-27 21:58:06 -0300

    Updated content

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Footer = props => {
   return (
     <footer className="d-flex justify-content-center">
-      Icons made by 
-      <a href="https://www.freepik.com" title="Freepik">Freepik</a> from 
-      <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      <p>
+      Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </p>
     </footer>
   );
 }
