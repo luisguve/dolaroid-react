e9c1ea4838dfd5c56d5e302f7a2be4082199e458
Luis G. Villegas <luisguve@gmail.com>
2021-06-02 18:44:33 -0300

    Moved definition of drag area component out to it's own file

---
+++ src/app/components/drag-modal.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { icons } from "../../assets";
+import { useToasts } from "react-toast-notifications";
+import vcf from "vcf";
+// Redux
+import { useDispatch } from "react-redux";
+import { loadContacts } from "../redux/actions";
+
+const Modal = props => {
+  const dispatch = useDispatch();
+  const dragText = "Arrastra el archivo aquí";
+  const releaseText = "Suelta el archivo para subirlo";
+  const [ headerText, setHeaderText ] = useState(dragText);
+  const [ dragAreaClass, setDragAreaClass ] = useState("drag-area");
+  const { addToast } = useToasts();
+
+  const handleFileContents = data => {
+    let vCard;
+    try {
+      vCard = vcf.parse(data);
+    } catch (err) {
+      console.log("Invalid vcard:", err);
+      addToast('archivo vCard inválido', { appearance: 'error' });
+    }
+    if (vCard) {
+      dispatch(loadContacts(vCard));
+      props.hide();
+    }
+  };
+
+  const readFile = file => {
+    if (file.type !== "text/x-vcard") {
+      addToast('Selecciona un archivo .VCF', { appearance: 'error' });
+      setHeaderText(dragText);
+      setDragAreaClass("drag-area");
+      return;
+    }
+    let reader = new FileReader();
+    reader.onload = () => {
+      handleFileContents(reader.result);
+    }
+    reader.readAsText(file);
+  };
+
+  let inputElement;
+  return (
+    <div className="modal" onClick={() => props.hide()}>
+      <div className={dragAreaClass}
+      onDragOver={e => {
+        e.preventDefault();
+        setDragAreaClass("drag-area active");
+        setHeaderText(releaseText);
+      }}
+      onDragLeave={() => {
+        setDragAreaClass("drag-area");
+        setHeaderText(dragText);
+      }}
+      onDrop={e => {
+        e.preventDefault();
+        setDragAreaClass("drag-area");
+        readFile(e.dataTransfer.files[0]);
+      }}
+      onClick={e => e.stopPropagation()}>
+        <img className="upload" alt="" src={icons.vcf} />
+        <header>{headerText}</header>
+        <span>o...</span>
+        <button onClick={() => inputElement.click()}>Selecciona el archivo</button>
+        <input type="file"
+          ref={input => inputElement = input} hidden
+          onChange={e => {
+            readFile(e.target.files[0]);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -2,30 +2,16 @@ import { icons } from "../../assets";
 import React from "react";
 import { useToasts } from "react-toast-notifications";
 import { useState } from "react";
+import Modal from "./drag-modal";
 // Redux
 import { useDispatch, useSelector } from "react-redux";
 import * as actions from "../redux/actions";
-// Package for reading VCF text.
-import vcf from "vcf";
 
 const Navbar = props => {
   let contacts = useSelector(state => state.contacts.contacts);
   const { addToast } = useToasts();
   const dispatch = useDispatch();
   const [showModal, setShowModal] = useState(false);
-  const handleFileContents = data => {
-    let vCard;
-    try {
-      vCard = vcf.parse(data);
-    } catch (err) {
-      console.log("Invalid vcard:", err);
-      addToast('archivo vCard inválido', { appearance: 'error' });
-    }
-    if (vCard) {
-      dispatch(actions.loadContacts(vCard));
-      setShowModal(false);
-    }
-  };
   const handleDownload = () => {
     if (!contacts.length) return;
 
@@ -104,63 +90,7 @@ const Navbar = props => {
           </div>
         </div>
       </nav>
-      {showModal && <Modal setFileContents={handleFileContents} hide={() => setShowModal(false)} />}
-    </div>
-  );
-};
-
-
-const Modal = props => {
-  const dragText = "Arrastra el archivo aquí";
-  const releaseText = "Suelta el archivo para subirlo";
-  const [ headerText, setHeaderText ] = useState(dragText);
-  const [ dragAreaClass, setDragAreaClass ] = useState("drag-area");
-  const { addToast } = useToasts();
-
-  const readFile = file => {
-    if (file.type !== "text/x-vcard") {
-      addToast('Selecciona un archivo .VCF', { appearance: 'error' });
-      setHeaderText(dragText);
-      setDragAreaClass("drag-area");
-      return;
-    }
-    let reader = new FileReader();
-    reader.onload = () => {
-      props.setFileContents(reader.result);
-    }
-    reader.readAsText(file);
-  };
-
-  let inputElement;
-  return (
-    <div className="modal" onClick={() => props.hide()}>
-      <div className={dragAreaClass}
-      onDragOver={e => {
-        e.preventDefault();
-        setDragAreaClass("drag-area active");
-        setHeaderText(releaseText);
-      }}
-      onDragLeave={() => {
-        setDragAreaClass("drag-area");
-        setHeaderText(dragText);
-      }}
-      onDrop={e => {
-        e.preventDefault();
-        setDragAreaClass("drag-area");
-        readFile(e.dataTransfer.files[0]);
-      }}
-      onClick={e => e.stopPropagation()}>
-        <img className="upload" alt="" src={icons.vcf} />
-        <header>{headerText}</header>
-        <span>o...</span>
-        <button onClick={() => inputElement.click()}>Selecciona el archivo</button>
-        <input type="file"
-          ref={input => inputElement = input} hidden
-          onChange={e => {
-            readFile(e.target.files[0]);
-          }}
-        />
-      </div>
+      {showModal && <Modal hide={() => setShowModal(false)} />}
     </div>
   );
 };
