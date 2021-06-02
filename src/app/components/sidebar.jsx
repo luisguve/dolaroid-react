b6252c3f80a69db5e9914a8d3f909dc69c298d03
Luis G. Villegas <luisguve@gmail.com>
2021-06-02 15:23:13 -0300

    Introduced button to display the editor on mobile screens and a button in the editor to delete a contact. Updated Editor to validate the phone number and display the save button if all the fields are valid

--- src/app/components/sidebar.jsx
+++ src/app/components/sidebar.jsx
@@ -4,12 +4,13 @@ import { icons } from "../../assets";
 import { useToasts } from "react-toast-notifications";
 // Redux
 import { useDispatch, useSelector } from "react-redux";
-import { addContact, editContact, getContact } from "../redux/actions";
+import { addContact, editContact, getContact, deleteContact } from "../redux/actions";
 import Footer from "./footer";
 
 const Sidebar = props => {
   const dispatch = useDispatch();
   const currentContact = useSelector(state => state.contacts.currentContact);
+  const listingBackgrounds = useSelector(state => state.settings.listBackgrounds);
   const { addToast } = useToasts();
 
   let heading = "Nuevo contacto";
@@ -81,22 +82,48 @@ const Sidebar = props => {
     info.set("n", n);
     info.set("tel", input.tel);
     dispatch(addContact(info));
+    // Hide Editor
+    setAddingNewContact(false);
     addToast('Contacto agregado', { appearance: 'success' });
   }
 
+  const handleDelete = () => {
+    dispatch(deleteContact(currentContact.idx));
+    setAddingNewContact(false);
+    addToast(currentContact.get("fn")._data + ' eliminado', { appearance: 'success' });
+  };
+
+  const [addingNewContact, setAddingNewContact] = useState(false);
+  // Display editor on mobile devices only if there is a contact to be edited.
+  let displayEditor = (currentContact || addingNewContact) ? " d-flex" : " d-none";
+
   return (
-    <div className="h-100 d-flex justify-content-center">
-      <aside className="p-3 pb-0 bg-transparent m-1 d-flex flex-column justify-content-between">
+    <div className="h-100 d-flex justify-content-md-center">
+      <aside className={"py-3 px-xl-5 pb-0 d-md-flex flex-column justify-content-between".concat(displayEditor)} >
         <Editor
           {...currentContactInfo}
+          editing={currentContact != null}
           heading={heading}
           handleSave={handleSave}
+          handleDelete={handleDelete}
           saveLabel={saveLabel}
-          resetInputs={() => {dispatch(getContact(-1))}}
+          resetInputs={() => {
+            dispatch(getContact(-1));
+            setAddingNewContact(false);
+          }}
           discardLabel={discardLabel}
         />
         <Footer />
       </aside>
+      {
+        !listingBackgrounds &&
+        <button
+          className="p-2 btn btn-primary rounded-circle d-md-none add"
+          onClick={() => setAddingNewContact(true)}
+        >
+          +
+        </button>
+      }
     </div>
   );
 };
@@ -106,16 +133,116 @@ const Editor = props => {
     photo: props.photo,
     fName: props.fName,
     lName: props.lName,
-    tel: props.tel,
+    tel: props.tel
   };
+  if ((contact.fName || contact.lName) && contact.tel) {
+    contact.valid = true;
+  }
   const [input, setInput] = useState(contact);
-  const dragText = "Arrastra una foto aquí";
-  const releaseText = "Suelta la foto";
-  const [photoText, setPhotoText] = useState(dragText);
-  const [dragAreaClass, setDragAreaClass] = useState("drag-area");
+  const [phone, setPhone] = useState({
+    label: "Teléfono:",
+    valid: true
+  });
+  const { addToast } = useToasts();
+  const validatePhone = number => {
+    if (!number) return {
+      label: "Teléfono:",
+      valid: true
+    };
+    // Does the phone number have just numbers?
+    if (/\D/.test(number)) {
+      return {
+        label: "El teléfono debe contener solo dígitos",
+        valid: false
+      };
+    }
+    switch(number.length) {
+    case 1:
+      // Does the phone number start with 0?
+      if (!/^0/.test(number)) {
+        return {
+          label: "El teléfono debe comenzar con 04(12/14/24/16/26)",
+          valid: false
+        };
+      }
+      break;
+    case 2:
+      // Does the phone number start with 04?
+      if (!/^04/.test(number)) {
+        return {
+          label: "El teléfono debe comenzar con 04(12/14/24/16/26)",
+          valid: false
+        };
+      }
+      break;
+    case 3:
+      // Does the phone number start with 04[1|2]?
+      if (!/^04[1|2]/.test(number)) {
+        return {
+          label: "El teléfono debe comenzar con 04(12/14/24/16/26)",
+          valid: false
+        };
+      }
+      break;
+    case 4:
+      // Does the phone number start with 04[1|2][2|4|6]?
+      if (!/^04[1|2][2|4|6]/.test(number)) {
+        return {
+          label: "El teléfono debe comenzar con 04(12/14/24/16/26)",
+          valid: false
+        };
+      }
+      break;
+    default:
+      // Does the phone number have more than 11 characters?
+      if (number.length > 11) {
+        return {
+          label: "El teléfono debe contener 11 dígitos",
+          valid: false
+        };
+      }
+    }
+    return {
+      label: "Teléfono:",
+      valid: true
+    };
+  };
+  const validatePhoneBlur = e => {
+    const number = e.target.value;
+    if (!number) return;
+    // Does the phone number have just numbers?
+    if (/\D/.test(number)) {
+      setPhone({
+        label: "El teléfono debe contener solo dígitos",
+        valid: false
+      });
+      return;
+    }
+    // Does the phone number start with 04[1|2][2|4|6]?
+    if (!/^04[1|2][2|4|6]/.test(number)) {
+      setPhone({
+        label: "El teléfono debe comenzar con 04(12/14/24/16/26)",
+        valid: false
+      });
+      return;
+    }
+    // Does the phone number have more than 11 characters?
+    if (number.length != 11) {
+      setPhone({
+        label: "El teléfono debe contener 11 dígitos",
+        valid: false
+      });
+      return;
+    }
+    setPhone({
+      label: "Teléfono:",
+      valid: true
+    });
+  }
   // Update input if a contact is being edited
   useEffect(() => {
     setInput(contact);
+    setPhone(validatePhone(contact.tel));
   }, [props]);
   const handleSave = () => {
     props.handleSave(input);
@@ -135,23 +262,32 @@ const Editor = props => {
       tel: ""
     });
   }
-  const handleChangeFName = e => {
-    setInput(Object.assign({}, input, {fName: e.target.value}));
-  };
   const handleChangePhoto = e => {
     setInput(Object.assign({}, input, {photo: e.target.value}));
   }
+  const handleChangeFName = e => {
+    let valid = ((e.target.value || input.lName) && (input.tel && phone.valid));
+    setInput(Object.assign({}, input, {fName: e.target.value, valid: valid}));
+  };
   const handleChangeLName = e => {
-    setInput(Object.assign({}, input, {lName: e.target.value}));
+    let valid = ((input.fName || e.target.value) && (input.tel && phone.valid));
+    setInput(Object.assign({}, input, {lName: e.target.value, valid: valid}));
   };
   const handleChangeTel = e => {
-    setInput(Object.assign({}, input, {tel: e.target.value}));
+    let phone = validatePhone(e.target.value);
+    let valid = ((input.fName || input.lName) && (e.target.value && phone.valid));
+    setInput(Object.assign({}, input, {tel: e.target.value, valid: valid}));
+    setPhone(phone);
+  };
+  const handleDelete = () => {
+    props.handleDelete();
   };
   let inputElement; // input type file
   const readFile = file => {
-    if (file.type !== "image/jpeg") {
-      console.log(file.type);
-      console.log('Selecciona un archivo JPG', { appearance: 'error' });
+    let fileType = file.type;
+    let validExtensions = ["image/jpeg", "image/jpg", "image/png"];
+    if (!validExtensions.includes(fileType)) {
+      addToast('Selecciona un archivo JPG o PNG', { appearance: 'error' });
       return;
     }
     let reader = new FileReader();
@@ -160,33 +296,35 @@ const Editor = props => {
     }
     reader.readAsDataURL(file);
   };
+  const dragText = "Arrastra una foto aquí";
+  const releaseText = "Suelta la foto";
+  const [photoTooltip, setPhotoTooltip] = useState(dragText);
+  const [dragAreaClass, setDragAreaClass] = useState("drag-area");
+
   return (
-    <div>
+    <div className="d-flex flex-column">
       <h4 className="text-center">{props.heading}</h4>
-      <div
-        className={"photo-container ".concat(dragAreaClass)}
+      <div className={"photo-container ".concat(dragAreaClass)}
         onDragOver={e => {
           e.preventDefault();
           setDragAreaClass("drag-area active");
-          setPhotoText(releaseText);
+          setPhotoTooltip(releaseText);
         }}
         onDragLeave={() => {
           setDragAreaClass("drag-area");
-          setPhotoText(dragText);
+          setPhotoTooltip(dragText);
         }}
         onDrop={e => {
           e.preventDefault();
           setDragAreaClass("drag-area");
-          setPhotoText(dragText);
+          setPhotoTooltip(dragText);
           readFile(e.dataTransfer.files[0]);
         }}
       >
-        <img className="w-100" height="220" src={input.photo} />
-        <p
-          onDragOver={e => e.stopPropagation()}
-          onDragLeave={e => e.stopPropagation()}
-          onDrop={e => e.stopPropagation()}
-        >{photoText}</p>
+        <img src={input.photo} />
+        <div className="photo-tooltip">
+          <p className="mb-0 mt-1">{photoTooltip}</p>
+        </div>
       </div>
       <button className="btn btn-secondary w-100 my-2"
         onClick={() => inputElement.click()}
@@ -199,41 +337,63 @@ const Editor = props => {
         }}
       />
       <label className="d-flex flex-column mb-2">
-        Nombre:
+        <span className="text-muted">Nombre:</span>
         <input
           type="text"
           placeholder="Nombre"
           value={input.fName}
           onChange={handleChangeFName}
+          className="form-control"
         />
       </label>
       <label className="d-flex flex-column mb-2">
-        Apellido:
+        <span className="text-muted">Apellido:</span>
         <input
           type="text"
           placeholder="Apellido"
           value={input.lName}
           onChange={handleChangeLName}
+          className="form-control"
         />
       </label>
       <label className="d-flex flex-column mb-2">
-        Teléfono:
+        <span className="text-muted">{phone.label}</span>
         <input
           type="text"
-          placeholder="Teléfono"
+          placeholder="Ej: 0424 906 9351"
           value={input.tel}
           onChange={handleChangeTel}
+          onBlur={validatePhoneBlur}
+          className={"form-control".concat(!phone.valid ? " invalid" : "")}
         />
       </label>
-      <div className="d-flex my-2">
-        <span className="w-50 pe-1"><button
-          className="btn btn-primary w-100"
-          onClick={handleSave}
-        >{props.saveLabel}</button></span>
-        <span className="w-50 ps-1"><button
-          className="btn btn-secondary w-100"
-          onClick={resetInputs}
-        >{props.discardLabel}</button></span>
+      <div className="d-flex flex-column my-2">
+        {
+          props.editing &&
+          <span className="w-100 mt-2 d-md-none"><button
+            className="btn btn-danger w-100"
+            onClick={handleDelete}
+          >Eliminar contacto</button></span>
+        }
+        <div className="d-flex mt-2">
+          <span className="w-50 pe-1"><button
+            className={
+              "btn btn-secondary w-100".concat(
+                (!(input.fName || input.lName || input.tel) && input.photo == icons.profilePic2) ?
+                " disabled" : ""
+              )
+            }
+            onClick={resetInputs}
+          >{props.discardLabel}</button></span>
+          <span className="w-50 mt-md-0 ps-1"><button
+            className={
+              "btn btn-primary w-100".concat(
+                !input.valid || !phone.valid || input.tel.length != 11 ? " disabled" : ""
+              )
+            }
+            onClick={handleSave}
+          >{props.saveLabel}</button></span>
+        </div>
       </div>
     </div>
   );
