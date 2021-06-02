0594402bd9e3e35d0d9ee05ccbf0384ad2fc615a
Luis G. Villegas <luisguve@gmail.com>
2021-06-02 18:44:35 -0300

    Display button to load vCard through drag area component

--- src/app/components/dashboard/list-contacts.jsx
+++ src/app/components/dashboard/list-contacts.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Contact from "./contact";
+import Modal from "../drag-modal";
 
 const ListContacts = props => {
+  const [showModal, setShowModal] = useState(false);
+
   let results = (
-    <div className="d-flex w-100 justify-content-center align-items-center">
+    <div className="d-flex flex-column w-100 justify-content-center align-items-center">
       <div className="p-3 no-contacts">
         <h1 className="fs-2">Carga un archivo vCard o crea contactos</h1>
         <p className="text-center fw-bold fs-6">Tus contactos aparecerán aquí</p>
       </div>
+      <button
+        className="btn btn-primary mt-3"
+        onClick={() => setShowModal(true)}
+      >Cargar vCard</button>
+      { showModal && <Modal hide={() => setShowModal(false)} /> }
     </div>
   );
 
