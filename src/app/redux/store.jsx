5d270c86e7a12b61beae2ac3d9c7907f1afb1a10
Luis G. Villegas <luisguve@gmail.com>
2021-06-02 20:18:20 -0300

    Load contacts from JS if vcard is unavailable

--- src/app/redux/store.jsx
+++ src/app/redux/store.jsx
@@ -3,19 +3,36 @@ import { createStore, combineReducers } from "redux";
 import { loadContacts } from "./actions";
 import vcf from "vcf";
 import vcfSample from "./contactos.vcf";
+import contacts from "./contacts";
 import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const store = createStore(combineReducers(reducers), devToolsEnhancer());
 
-let vCards;
-try {
-  vCards = vcf.parse(vcfSample);
-} catch(err) {
-  console.log(err);
+let vCards = [];
+let loadedFrom;
+if (!vcfSample) {
+  // Create contacts
+  contacts.map(contact => {
+    let info = new vcf();
+    info.set("fn", contact.fn);
+    info.set("photo", contact.photo);
+    info.set("n", contact.n);
+    info.set("tel", contact.number);
+    vCards.push(info);
+  });
+  loadedFrom = "Loaded contacts from js";
+} else {
+  try {
+    vCards = vcf.parse(vcfSample);
+  } catch(err) {
+    console.log(err);
+  }
+  loadedFrom = "Loaded contacts from vCard";
 }
 
-if (vCards) {
+if (vCards.length) {
   store.dispatch(loadContacts(vCards));
+  console.log(loadedFrom);
 }
 
 export default store;
