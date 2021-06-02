54802154eb0ce06b3683e95ef866f08cdff3bc85
Luis G. Villegas <luisguve@gmail.com>
2021-06-02 19:34:35 -0300

    Go back to previous state

--- src/app/redux/store.jsx
+++ src/app/redux/store.jsx
@@ -1,12 +1,12 @@
 import * as reducers from './reducers';
 import { createStore, combineReducers } from "redux";
-//import { loadContacts } from "./actions";
-//import vcf from "vcf";
-//import vcfSample from "./contactos.vcf";
+import { loadContacts } from "./actions";
+import vcf from "vcf";
+import vcfSample from "./contactos.vcf";
 import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const store = createStore(combineReducers(reducers), devToolsEnhancer());
-/*
+
 let vCards;
 try {
   vCards = vcf.parse(vcfSample);
@@ -17,5 +17,5 @@ try {
 if (vCards) {
   store.dispatch(loadContacts(vCards));
 }
-*/
+
 export default store;
