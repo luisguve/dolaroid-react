8a9f547e61974cdfa44d1dbcd4b370bb68353089
Luis G. Villegas <luisguve@gmail.com>
2021-06-10 17:52:56 -0300

    Wrap fetching of contacts from localStorage into the promise returned

--- src/app/redux/load-contacts.jsx
+++ src/app/redux/load-contacts.jsx
@@ -31,21 +31,21 @@ async function loadExample() {
 };
 
 const loadDefaultContacts = () => {
-  let vCards;
-  // Try to fetch contacts from local storage.
-  if (typeof(Storage) !== "undefined") {
-    const vcfContacts = localStorage.getItem("contacts");
-    if (vcfContacts) {
-      if (vcfContacts === "empty") {
-        // The user explicitly removed all the contacts.
-        return null;
-      }
-      vCards = vcf.parse(vcfContacts);
-      console.log("Loaded contacts from localStorage");
-    }
-  }
-
   return new Promise(resolve => {
+    let vCards;
+    // Try to fetch contacts from local storage.
+    if (typeof(Storage) !== "undefined") {
+      const vcfContacts = localStorage.getItem("contacts");
+      if (vcfContacts) {
+        if (vcfContacts === "empty") {
+          // The user explicitly removed all the contacts.
+          resolve(null);
+          return;
+        }
+        vCards = vcf.parse(vcfContacts);
+        console.log("Loaded contacts from localStorage");
+      }
+    }
     if (!vCards) {
       loadExample().then(vCards => {
         resolve(vCards)
