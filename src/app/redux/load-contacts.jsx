a1e117eccb6e964a9bb4f236beae11509baad1c3
Luis G. Villegas <luisguve@gmail.com>
2021-06-10 19:32:48 -0300

    Removed logic to get example contacts from VCF file

--- src/app/redux/load-contacts.jsx
+++ src/app/redux/load-contacts.jsx
@@ -1,32 +1,21 @@
 import vcf from "vcf";
-// Dynamically import VCF sample file
-const getVcfSample = () => import("./contactos.vcf");
 // Dynamically import contacts from JS
 const getContacts = () => import("./contacts");
 
 async function loadExample() {
-  let vCards = [];
-  let loadedFrom;
-  try {
-    const vcfSample = await getVcfSample();
-    vCards = vcf.parse(vcfSample.default);
-    loadedFrom = "Loaded contacts from vCard";
-  } catch(err) {
-    console.log(err);
-    // Failed to load vCard
-    // Create contacts from JS
-    const contacts = await getContacts();
-    contacts.default.map(contact => {
-      let info = new vcf();
-      info.set("fn", contact.fn);
-      info.set("photo", contact.photo);
-      info.set("n", contact.n);
-      info.set("tel", contact.number);
-      vCards.push(info);
-    });
-    loadedFrom = "Loaded contacts from JS";
-  }
-  console.log(loadedFrom);
+  const vCards = [];
+  // Get contacts as an array of JSON
+  const contacts = await getContacts();
+  // Create contacts from array of JSON
+  contacts.default.map(contact => {
+    let info = new vcf();
+    info.set("fn", contact.fn);
+    info.set("photo", contact.photo);
+    info.set("n", contact.n);
+    info.set("tel", contact.number);
+    vCards.push(info);
+  });
+  console.log("Loaded contacts from JS");
   return vCards;
 };
 
